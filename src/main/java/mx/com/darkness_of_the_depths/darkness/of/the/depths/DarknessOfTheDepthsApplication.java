12620ef3beee83280d7cede4e8632d757cfcef20
Saul Medina Osornio <SaulMedinaOsornio@outlook.com>
2024-07-03 17:50:48 -0600

    the creation of the structure proyect

---
+++ src/main/java/mx/com/darkness_of_the_depths/darkness/of/the/depths/DarknessOfTheDepthsApplication.java
@@ -0,0 +1,13 @@
+package mx.com.darkness_of_the_depths.darkness.of.the.depths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarknessOfTheDepthsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarknessOfTheDepthsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/darkness_of_the_depths/darkness/of/the/depths/DarknessOfTheDepthsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.darkness_of_the_depths.darkness.of.the.depths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarknessOfTheDepthsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
